a743cf5d1e90b07def620052a6bb479c6b042321
tgarvs <tgarvey12@gmail.com>
2025-08-25 13:21:24 +0100

    adding fireflies and sound

---
+++ ClearCut_C++Port/Fireflies.hpp
@@ -0,0 +1,13 @@
+//
+//  Fireflies.hpp
+//  ClearCut_C++Port
+//
+//  Created by Thomas Garvey on 8/23/25.
+//
+
+#ifndef Fireflies_hpp
+#define Fireflies_hpp
+
+#include <stdio.h>
+
+#endif /* Fireflies_hpp */
